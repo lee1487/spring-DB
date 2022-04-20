a9cc90ad7961bc826de0f6ea3c7c3dae2be4c037
이현세 <dlgustp1487@naver.com>
2022-04-20 13:27:41 +0900

    트랜잭션 문제 해결 - 트랜잭션 템플릿

--- src/test/java/hello/jdbc/service/MemberServiceV3_2Test.java
+++ src/test/java/hello/jdbc/service/MemberServiceV3_2Test.java
@@ -23,24 +23,24 @@ import hello.jdbc.repository.MemberRepositoryV2;
 import hello.jdbc.repository.MemberRepositoryV3;
 
 /**
- * 트랜잭션 - 트랜잭션 매니저 
+ * 트랜잭션 - 트랜잭션 템플릿 
  */
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class MemberServiceV3_1Test {
+class MemberServiceV3_2Test {
 
 	public static final String MEMBER_A = "memberA";
 	public static final String MEMBER_B = "memberB";
 	public static final String MEMBER_EX = "ex";
 	
 	private MemberRepositoryV3 memberRepository;
-	private MemberServiceV3_1 memberService;
+	private MemberServiceV3_2 memberService;
 	
 	@BeforeEach
 	void before() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
 		memberRepository = new MemberRepositoryV3(dataSource);
 		DataSourceTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);
-		memberService = new MemberServiceV3_1(transactionManager, memberRepository);
+		memberService = new MemberServiceV3_2(transactionManager, memberRepository);
 	}
 	
 	@AfterEach
